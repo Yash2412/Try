38cef4f09de463383808199b29ce5bbc50279c31
Yash2412 <yashjanoria111@gmail.com>
2022-03-10 14:24:44 +0530

    Bug

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App () {
    return (
       <div className="App">
-         Lodu lassonjksnbdsalkjfbdfasfhbdsajfgjdsafhdsa
+         Maa chudae duniya
       </div>
    );
 }
