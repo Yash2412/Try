309b7a8d4ceebc746126eac2b8431737145a3d2e
Yash2412 <yashjanoria111@gmail.com>
2022-03-10 14:03:24 +0530

    Changed outside wala app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Testing Github
-    </div>
-  );
+function App () {
+   return (
+      <div className="App">
+         Lodu lassonjksnbdsalkjfbdfasfhbdsajfgjdsafhdsa
+      </div>
+   );
 }
 
 export default App;
